52857eef774fba95648f5cd9ab623a05079dd07a
patten03 <patten010203@gmail.com>
2024-07-08 23:23:16 +0300

    split interface functions

--- interface.cpp
+++ interface.cpp
@@ -1,53 +1,75 @@
 #include "interface.h"
 
-void Configuration::inputICAO()
-{
-	bool done(false);        // loop value
-	uint8_t iterator(0);     // index of digit (left to rigth 0-5)
+void Configuration::inputICAO() {
 	uint8_t address[]{ 0,0,0,0,0,0 };
 	uint8_t base(16);        // hexademical base
 
+	controlPanel(address, sizeof(address), base);
+
+}
+
+void showInput(const uint8_t data[], const uint8_t dataLength, const uint8_t base) {
+	system("cls");
+
+	for (int i = 0; i < dataLength; i++) {
+		printf("%X ", data[i]);
+	}
+}
+
+uint8_t receiveKey() {
+	uint8_t res = _getch();
+	return res;
+}
+
+void controlPanel(uint8_t data[], const uint8_t dataLength, const uint8_t base) {
+
+	bool done(false);        // loop value
+	uint8_t iterator(0);     // index of digit (left to rigth)
+
 	while (!done) {
 		// show the current result
-		system("cls");
-		printf("%x %x %x %x %x %x\n",
-			address[0], address[1], address[2], address[3], address[4], address[5]);
+		showInput(data, dataLength, base);
 
 		uint8_t key;
-		key = _getch();
-		
+		key = receiveKey();
+
 		switch (key) {
-		case encoderEnter: {
-			if (iterator >= sizeof(address) - 1) // looping input of digits
+		case encoderEnter:
+		{
+			if (iterator >= dataLength - 1) // looping input of digits
 				iterator = 0;
 			else
 				iterator++;
 		} break;
 
-		case encoderLeft: {
-			if (address[iterator] <= 0)
-				address[iterator] = base - 1;
+		case encoderLeft:
+		{
+			if (data[iterator] <= 0)
+				data[iterator] = base - 1;
 			else
-				address[iterator]--;
+				data[iterator]--;
 		} break;
 
-		case encoderRight: {
-			if (address[iterator] >= base - 1) 
-				address[iterator] = 0;
+		case encoderRight:
+		{
+			if (data[iterator] >= base - 1)
+				data[iterator] = 0;
 			else
-				address[iterator]++;
+				data[iterator]++;
 		} break;
 
-		case FNK: {
+		case FNK:
+		{
 			done = true;
 		} break;
-			
-		case FNK_hold10sec: {
+
+		case FNK_hold10sec:
+		{
 
 			// TODO some functionality
 
 		} break;
-		
+
 		default:
 			break;
 		}
--- interface.h
+++ interface.h
@@ -7,19 +7,22 @@
 
 // TODO after import to keil you have to change keys values
 enum keys {
-	SIGN,                                          // (ZNAK)
-	INPUT,                                         // (VVOD)
-	VFR,                                           // visual flight rules (PVP)
-	FNK = 27, FNK_hold10sec,                       // What's a FNK button on controller?
-	encoderLeft = 'a', encoderRight = 'd', encoderEnter = 13
+	SIGN,                                                    // (ZNAK)
+	INPUT,                                                   // (VVOD)
+	VFR,                                                     // visual flight rules (PVP)
+	FNK = 27, FNK_hold10sec,                                 // functional button
+	encoderLeft = 'a', encoderRight = 'd', encoderEnter = 13 // encoder
 };
 
-struct Configuration
-{
+struct Configuration {
 	uint32_t ICAO;
 
 	void inputICAO();
 };
 
+
+void showInput(const uint8_t data[], const uint8_t dataLength, const uint8_t base);
+uint8_t receiveKey();
+void controlPanel(uint8_t data[], const uint8_t dataLength, const uint8_t base);
 
 #endif // INTERFACE
